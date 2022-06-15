fd1d8fc136b8d0a7cbb43beb8c3a69efa06ce9f2
xuelin-xie <85014785+xuelin-xie@users.noreply.github.com>
2022-06-15 15:57:33 +0800

    Add files via upload

--- buildKRG.m
+++ buildKRG.m
@@ -1,12 +1,15 @@
 function krgmodel = buildKRG(S,Y)
 %  Build a surrogate function based on Kriging Functions
 [~,dim]=size(S);
+%初始化Kriging的相关条件
 theta = 10; 
 lob = 1e-1;
 upb = 100;   
 theta = repmat(theta, 1, dim) ;
 lob = repmat(lob, 1, dim);
 upb =  repmat(upb, 1, dim);
+
+% 建立Kriging模型
 % krgmodel =dacefit(S, Y, @regpoly0, @corrgauss, theta, lob, upb);
 krgmodel =dacefit(S,Y,'regpoly2','corrgauss', theta, lob, upb);
 return
